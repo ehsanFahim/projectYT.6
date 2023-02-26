fd12fe00952b4ae39feaf699e79c526ce28b255b
fahim <ehsanulhoquefahim@gmail.com>
2023-02-27 00:05:30 +0600

    nada

---
+++ main.js
@@ -0,0 +1,69 @@
+//Selectors
+const todoInput = document.querySelector('.todo-text')
+const todoButton = document.querySelector('.todo-btn')
+const todoList = document.querySelector('.todo-list')
+const filterOption = document.querySelector('.filter-todo')
+
+//Event Listener
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click',filterTodo)
+
+//Function
+function addTodo(event) {
+    //Prevent form frpm submitting
+
+    event.preventDefault();
+
+    //todo div
+    const todoDiv = document.createElement('div')
+    todoDiv.classList.add('todo')
+
+    //create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo)
+
+    //create Mark button
+
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fa fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton)
+
+    //create Mark button
+
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fa fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton)
+
+    //Append to list
+    todoList.appendChild(todoDiv);
+    //Clear Todo input
+    todoInput.value = '';
+
+}
+function deleteCheck(e) {
+    const item = e.target;
+    //DELETE TODO
+    if (item.classList[0] === "trash-btn") {
+        const todo = item.parentElement;
+        //Animation
+        todo.classList.add("fall");
+        todo.addEventListener("transitionend", function () {
+            todo.remove();
+        })
+    }
+    if (item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed')
+    }
+}
+
+function filterTodo(e){
+ const todos = todoList.childNodes;
+ 
+ console.log(todos)
+}
